6dd3629d08371fba04703fd8fc2f6a4b0265f7bf
Andrew Joubert <andrew@siliconrhino.io>
2021-02-26 15:48:29 +0000

    Add mouse drag mouse event

--- src/components/DrawingCanvas/DrawingCanvas.tsx
+++ src/components/DrawingCanvas/DrawingCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PaperCanvas from "../PaperCanvas/PaperCanvas";
 import "./DrawingCanvas.scss";
 
@@ -15,6 +15,7 @@ const DrawingCanvas: React.FC<Props> = (props) => {
   const { paperScope, tool } = props;
   let paperProject: paper.Project;
   console.log("PaperCanvas", paperScope, canvasRef, tool);
+  let selectedItem: paper.Path.Circle | paper.Path.Rectangle | null;
 
   const setupPaperProject = () => {
     paperProject = paperScope.project;
@@ -34,6 +35,8 @@ const DrawingCanvas: React.FC<Props> = (props) => {
   useEffect(() => {
     if (paperScope.view) {
       paperScope.view.onMouseUp = onMouseUp;
+      paperScope.view.onMouseDown = onMouseDown;
+      paperScope.view.onMouseDrag = onMouseDrag;
     }
     console.log("Setup Events", tool);
   }, [tool]);
@@ -43,22 +46,35 @@ const DrawingCanvas: React.FC<Props> = (props) => {
   });
 
   const drawCircle = (e: paper.MouseEvent) => {
-    const circle = paperProject.activeLayer.addChild(
-      new paperScope.Shape.Circle(e.point, 20)
-    );
+    const circle = new paperScope.Path.Circle(e.point, 20);
+    circle.translate(new paperScope.Point([-20, -20]));
     circle.strokeWidth = 1;
     circle.strokeColor = new paperScope.Color("Black");
+    circle.selected = true;
+
+    paperProject.activeLayer.addChild(circle);
+    selectedItem = circle;
   };
 
   const drawRectangle = (e: paper.MouseEvent) => {
-    const rectangle = paperProject.activeLayer.addChild(
-      new paperScope.Shape.Rectangle(e.point, new paperScope.Size(20, 20))
+    const rectangle = new paperScope.Path.Rectangle(
+      e.point,
+      new paperScope.Size(50, 50)
     );
+    rectangle.translate(new paperScope.Point([-50, -50]));
     rectangle.strokeWidth = 1;
     rectangle.strokeColor = new paperScope.Color("Black");
+
+    paperProject.activeLayer.addChild(rectangle);
+    selectedItem = rectangle;
   };
 
-  const onMouseUp = (event: any) => {
+  const onMouseUp = (_event: paper.MouseEvent) => {
+    if (selectedItem) selectedItem.selected = false;
+    selectedItem = null;
+  };
+
+  const onMouseDown = (event: paper.MouseEvent) => {
     switch (tool) {
       case "Circle":
         drawCircle(event);
@@ -66,6 +82,15 @@ const DrawingCanvas: React.FC<Props> = (props) => {
       case "Rectangle":
         drawRectangle(event);
         break;
+    }
+  };
+
+  const onMouseDrag = (event: paper.MouseEvent) => {
+    if (selectedItem) {
+      selectedItem.bounds.width += event.delta.x;
+      selectedItem.bounds.height += event.delta.y;
+    } else {
+      console.log("no selected item");
     }
   };
 
